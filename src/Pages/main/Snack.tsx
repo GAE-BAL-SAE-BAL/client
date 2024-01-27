df8c53a000664e49b5f672262fe8fe16220b0767
Ubinquitous <ubinquitous1@gmail.com>
2024-01-27 21:18:23 +0900

    지금이번호로저나해줘~~~콜미백

--- src/Pages/main/Snack.tsx
+++ src/Pages/main/Snack.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNavigation from "../../components/BottomNavigation";
-import Logo from "../../assets/Logo";
-import Person from "../../assets/Person";
 import FoodCard from "../../components/FoodCard";
 import Header from "../../components/Header";
+import SearchIcon from "../../assets/SearchIcon";
+import RightArrowBig from "../../assets/RightArrowBig";
+import SnackNotFound from "../../assets/SnackNotFound";
 
 const Snack = () => {
+  const [result, setResult] = useState(["", "", ""]);
+  // const [result, setResult] = useState([]);
   return (
     <div className="w-full px-[23px] gap-8 py-12 pb-24 flex flex-col">
       <Header />
@@ -14,16 +17,49 @@ const Snack = () => {
         <br />
         다양한 안주를 만나보세요
       </span>
-      <main className="flex flex-wrap w-full gap-[19px]">
-        {Array.from({ length: 20 }).map((_, i) => (
-          <FoodCard
-            key={i}
-            src="/mock_snack.png"
-            name="손쉽게 구워 맛있게 먹는 소고기 스테이크"
-            price={24580}
-          />
-        ))}
-      </main>
+      <div className="relative h-[58px]">
+        <input
+          className="p-[16px] absolute w-full rounded-[12px] border-[1px] border-solid border-[#D0D1D2]"
+          placeholder="안주를 검색해보세요"
+        />
+        <SearchIcon className="absolute right-4 top-[17px]" />
+      </div>
+      {result.length ? (
+        <main className="flex flex-wrap w-full gap-[19px]">
+          {Array.from({ length: 6 }).map((_, i) => (
+            <FoodCard
+              key={i}
+              src="/mock_snack.png"
+              name="손쉽게 구워 맛있게 먹는 소고기 스테이크"
+              price={24580}
+            />
+          ))}
+          <div className="w-full p-[20px] flex items-center justify-between bg-black rounded-[12px]">
+            <span className="text-[16px] font-[600] text-white ">
+              내가 고른 안주랑
+              <br />잘 어울리는 주류 보러가기
+            </span>
+            <RightArrowBig />
+          </div>
+          {Array.from({ length: 6 }).map((_, i) => (
+            <FoodCard
+              key={i}
+              src="/mock_snack.png"
+              name="손쉽게 구워 맛있게 먹는 소고기 스테이크"
+              price={24580}
+            />
+          ))}
+        </main>
+      ) : (
+        <main className="w-full h-[50vh] flex flex-col gap-[12px] items-center justify-center">
+          <SnackNotFound />
+          <span className="text-[16px] font-[400] text-center text-[#B9BBB9]">
+            해당 안주 상품이 없어요.
+            <br />
+            오타나 띄어쓰기 등을 점검해주세요.
+          </span>
+        </main>
+      )}
       <BottomNavigation current="안주추천" />
     </div>
   );
---
+++ src/assets/RightArrowBig.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const RightArrowBig = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_68_3985)">
+        <path
+          d="M9 5L16.1399 11.2474C16.5952 11.6458 16.5952 12.3542 16.1399 12.7526L9 19"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_68_3985">
+          <rect
+            width="10"
+            height="16"
+            fill="white"
+            transform="translate(8 4)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default RightArrowBig;
