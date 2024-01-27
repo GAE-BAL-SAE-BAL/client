ae9125c20b17f852387ac8efd264b51bcf821edb
KANGYONGSU23 <edc20203@gmail.com>
2024-01-27 18:55:48 +0900

    회원가입-ID입력

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IdInputPage from "./page/IdInputPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<></>} />
-        <Route path="/" element={<></>} />
+        <Route path="/signup" element={<IdInputPage />} />
         <Route path="/" element={<></>} />
         <Route path="/" element={<></>} />
         <Route path="/" element={<></>} />
